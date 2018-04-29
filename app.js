5f7ada1c139270141c9496a97c13486e57e8bb12
Jak Ashuraliev <ashuraliev@gmail.com>
2018-04-29 02:10:54 +0000

    modified .gitignore file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app         = express();
 var bodyParser  = require('body-parser');
 var mongoose    = require('mongoose');
 
-// Genre = require('./models/genre');
+Genre = require('./models/genre');
 
 // Connect to Mongoose
 mongoose.connect('mongodb://localhost/bookstore');
@@ -13,14 +13,15 @@ app.get('/', function(req, res){
    res.send('Hello World one more time!'); 
 });
 
-// app.get('/api/genres', function(req, res){
-//    Genre.getGenres(function(err, genres){
-//       if(err){
-//          throw err;
-//       }
-//       res.jason(genres);
-//    });
-// });
+app.get('/api/genres', function(req, res){
+   Genre.getGenres(function(err, genres){
+      if(err){
+         throw err;
+      }
+      res.jason(genres);
+   });
+});
+
 
 app.listen(process.env.PORT, process.env.IP);
 console.log('App is and running!');
