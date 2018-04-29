7c304bd52f9a6d73d892e30b5f43f3892101f8ac
Jak Ashuraliev <ashuraliev@gmail.com>
2018-04-29 01:55:19 +0000

    Set up /api/genres rout and genre.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app         = express();
 var bodyParser  = require('body-parser');
 var mongoose    = require('mongoose');
 
+// Genre = require('./models/genre');
+
 // Connect to Mongoose
 mongoose.connect('mongodb://localhost/bookstore');
 var db = mongoose.connection;
@@ -11,9 +13,14 @@ app.get('/', function(req, res){
    res.send('Hello World one more time!'); 
 });
 
-app.get('/api/genres', function(req, res){
-   res.send();
-});
+// app.get('/api/genres', function(req, res){
+//    Genre.getGenres(function(err, genres){
+//       if(err){
+//          throw err;
+//       }
+//       res.jason(genres);
+//    });
+// });
 
 app.listen(process.env.PORT, process.env.IP);
 console.log('App is and running!');
--- models/genre.js
+++ models/genre.js
@@ -2,12 +2,19 @@ var mongoose = require('mongoose');
 
 // Genre Scheme
 var genreSchema = mongoose.Schema({
-    name:{
-        type: String,
-        required: true
-    },
-    crateDate:{
-        type: Date,
-        default: Date.now
-    }
+   name:{
+       type: String,
+       required: true
+   },
+   createDate:{
+       type: Date,
+       default: Date.now
+   }
 });
+
+var Genre = module.exports = mongoose.model('Genre', genreSchema);
+
+// Funciont to Get Genres
+module.exports.getGenres = function(callback, limit){
+    Genre.find(callback).limit(limit);
+}
